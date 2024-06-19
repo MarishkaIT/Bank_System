4f7aa4db1b99cc262dc2e2ae39a9df3863c44e83
marishka_rish <litvinskaya.rish@gmail.com>
2024-06-19 16:10:06 +0300

    created service

--- src/main/java/com/bank/bank_system/service/AccountService.java
+++ src/main/java/com/bank/bank_system/service/AccountService.java
@@ -2,12 +2,17 @@ package com.bank.bank_system.service;
 
 import com.bank.bank_system.entity.Account;
 import com.bank.bank_system.entity.Client;
+import com.bank.bank_system.exception.AccountNotFoundException;
 import com.bank.bank_system.exception.ClientNotFoundException;
+import com.bank.bank_system.exception.InsufficientBalanceException;
 import com.bank.bank_system.repository.AccountRepository;
 import com.bank.bank_system.repository.ClientRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class AccountService {
@@ -17,8 +22,55 @@ public class AccountService {
 
     @Transactional
     public Account createClientAccount(Long clientId, Account account) {
-        Client client = clientRepository.findById(clientId).orElseThrow(() -> new ClientNotFoundException("Client not found!"));
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(() -> new ClientNotFoundException("Client not found!"));
         account.setClient(client);
         return accountRepository.save(account);
     }
+
+    public Account getAccount(Long id) {
+        return accountRepository.findById(id)
+                .orElseThrow(()-> new AccountNotFoundException("Account not found!"));
+    }
+
+    public List<Account> getAccountsByClient(Long clientId) {
+        Client client = clientRepository.findById(clientId)
+                .orElseThrow(() -> new AccountNotFoundException("Account not found!"));
+        return accountRepository.findByClient(client);
+    }
+
+    @Transactional
+    public Account updateAccount(Account account) {
+        return accountRepository.save(account);
+    }
+
+    @Transactional
+    public void deleteAccount(Long accountId) {
+        accountRepository.deleteById(accountId);
+    }
+
+    public Account getAccountById(Long accountId) {
+        return accountRepository.findById(accountId)
+                .orElseThrow(()-> new AccountNotFoundException("Account not found!"));
+    }
+
+    public void withdraw(Long accountId, BigDecimal amount) {
+        Account account = getAccount(accountId);
+
+        if (account.getBalance().compareTo(amount) < 0) {
+            throw new InsufficientBalanceException(accountId, amount);
+        }
+        account.setBalance(account.getBalance().subtract(amount));
+        accountRepository.save(account);
+    }
+
+    public void deposit(Long accountId, BigDecimal amount) {
+        Account account = getAccount(accountId);
+        account.setBalance(account.getBalance().add(amount));
+        accountRepository.save(account);
+    }
+     public void transfer(Long fromAccountId, Long toAccountId, BigDecimal amount) {
+        withdraw(fromAccountId, amount);
+        deposit(toAccountId, amount);
+     }
 }
