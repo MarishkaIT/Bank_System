3866335b2e00172f39ad3e2ceda3fc397893b2e2
marishka_rish <litvinskaya.rish@gmail.com>
2024-06-19 16:11:19 +0300

    repository

--- src/main/java/com/bank/bank_system/repository/AccountRepository.java
+++ src/main/java/com/bank/bank_system/repository/AccountRepository.java
@@ -1,7 +1,13 @@
 package com.bank.bank_system.repository;
 
 import com.bank.bank_system.entity.Account;
+import com.bank.bank_system.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByClientId(Long clientId);
+
+    List<Account> findByClient(Client client);
 }
