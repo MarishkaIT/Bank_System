4d160f6bd8e20c771a39082d333db046685fc5ea
marishka_rish <litvinskaya.rish@gmail.com>
2024-06-28 13:12:10 +0300

    refactoring

--- src/main/java/com/bank/bank_system/entity/Transaction.java
+++ src/main/java/com/bank/bank_system/entity/Transaction.java
@@ -14,6 +14,7 @@ import java.time.Instant;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Table(name = "transaction")
 public class Transaction {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
